f2854c13329a7133bce6ae9b31027ba3ae617857
Kat Berton <katherine.berton@gmail.com>
2022-03-22 12:04:30 -0700

    added static folder

---
+++ static/js/tracker.js
@@ -0,0 +1,20 @@
+/*
+
+fetch('/some-url')
+  .then(response => response.json())
+  .then(responseData => {
+    document.querySelector('#my-div').innerText = responseData;
+  });
+
+
+generator for cantrip buttons
+command to generate the cantrip buttons
+
+generator for slot buttons
+command to generate the slot buttons
+
+fetch request to retrieve slot deets
+
+*/
+
+
